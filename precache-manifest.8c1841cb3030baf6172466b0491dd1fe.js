16395993da7de6f739f39067e1e4e871767ad63f
Denichandrap <app.denichandrap@gmail.com>
2020-10-17 15:53:32 +0700

    Updates

---
+++ precache-manifest.8c1841cb3030baf6172466b0491dd1fe.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336930d1e93634cdf9cb622a3ca7da0b",
+    "url": "/undangan-react/index.html"
+  },
+  {
+    "revision": "a60ceb0a8841636963f9",
+    "url": "/undangan-react/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "2c39c5e078ccb5428da3",
+    "url": "/undangan-react/static/css/main.06bebe96.chunk.css"
+  },
+  {
+    "revision": "a60ceb0a8841636963f9",
+    "url": "/undangan-react/static/js/2.b87b1621.chunk.js"
+  },
+  {
+    "revision": "5ad387cd57eb31f6f50768be47fe3b09",
+    "url": "/undangan-react/static/js/2.b87b1621.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c39c5e078ccb5428da3",
+    "url": "/undangan-react/static/js/main.95d67c9e.chunk.js"
+  },
+  {
+    "revision": "5bc6f84bc2edea8eae8b",
+    "url": "/undangan-react/static/js/runtime-main.4f05fc04.js"
+  },
+  {
+    "revision": "8fedcc3b4ba4c2272d4071736ff22039",
+    "url": "/undangan-react/static/media/2an.8fedcc3b.png"
+  },
+  {
+    "revision": "5ae610744bc90ffde0ec1f1b06ef2fa9",
+    "url": "/undangan-react/static/media/Mogalaris.5ae61074.otf"
+  },
+  {
+    "revision": "8198e978747df98a2c4774a24a4528cf",
+    "url": "/undangan-react/static/media/SweetsDelight.8198e978.otf"
+  },
+  {
+    "revision": "b473f9790f9c5b8273f48a80f53119de",
+    "url": "/undangan-react/static/media/deni.b473f979.png"
+  },
+  {
+    "revision": "39b3eafca1c57a56ddfcc9db584324e6",
+    "url": "/undangan-react/static/media/gusti.39b3eafc.png"
+  },
+  {
+    "revision": "61dca8833222d780124d4633f7e75849",
+    "url": "/undangan-react/static/media/ima.61dca883.png"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/undangan-react/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/undangan-react/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "e0971e343268892eca2d7596805a669a",
+    "url": "/undangan-react/static/media/rose.e0971e34.png"
+  },
+  {
+    "revision": "0e95efabba5425a62ade96444cda8495",
+    "url": "/undangan-react/static/media/test1.0e95efab.jpg"
+  }
+]);
